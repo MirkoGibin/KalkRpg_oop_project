f1543a9530e019ceb5db60773aae9120ba9d8a8e
Mirko Gibin <mirkogibin@gmail.com>
2018-01-22 16:06:59 +0100

    sistemati clear memory e back, nuovi inserimenti vanno sul fondo del display. Scroll da sistemare

--- tempIdeas/untitled/display.h
+++ tempIdeas/untitled/display.h
@@ -3,10 +3,15 @@
 
 #include<QWidget>
 #include<QTextEdit>
+#include<QList>
+#include<QTextTable>
+#include<QTextCursor>
+#include<QScrollBar>
 
 
 class Display : public QTextEdit {
 private:
+    QList<QTextCursor> history;
 public:
     Display(QWidget* parent =0) : QTextEdit(parent) {
         setReadOnly(true);
@@ -16,29 +21,63 @@ public:
     void show(const QImage* image, QList<QString>* parametri) {
 
         QTextCursor cursor = textCursor();
-        cursor.setPosition(0);
 
-        QTextTableFormat format;
-        format.setBorder(0);
+        QTextTableFormat tabFormat;
+        tabFormat.setBorder(0);
+        tabFormat.setAlignment(Qt::AlignCenter);
 
         QVector<QTextLength> cellWidth;
         for(int i = 0; i < parametri->length(); i++)
             cellWidth.push_back(QTextLength(QTextLength::FixedLength, 70));
-        format.setColumnWidthConstraints(cellWidth);
+        tabFormat.setColumnWidthConstraints(cellWidth);
 
-        QTextTable* table = cursor.insertTable(1,parametri->length()+1, format);
+        QTextTable* table = cursor.insertTable(1,parametri->length()+1, tabFormat);
+
+        history.push_back(cursor);
 
         table->cellAt(0, 0).firstCursorPosition().insertImage(*image);
 
         int j=1;
         while(!(parametri->isEmpty())) {
-            table->cellAt(0, j).
-                    firstCursorPosition().
-                    insertText(parametri->takeFirst());
+            QTextCursor tabCursor = table->cellAt(0, j).firstCursorPosition();
+            QTextBlockFormat align;
+            align.setAlignment(Qt::AlignHCenter);
+            tabCursor.setBlockFormat(align);
+            tabCursor.insertText(parametri->takeFirst());
             j++;
         }
-
         delete parametri;
+        moveCursor(QTextCursor::End);
+        ensureCursorVisible();
+    }
+
+    void show(const char* path) {
+        QTextCursor cursor = textCursor();
+
+        QTextBlockFormat format = cursor.blockFormat();
+        format.setAlignment(Qt::AlignCenter);
+        cursor.mergeBlockFormat(format);
+
+        QImage image(path);
+        history.push_back(cursor);
+        cursor.insertImage(image);
+
+        moveCursor(QTextCursor::End);
+    }
+
+    void back() {
+        if(!(history.empty())) {
+            QTextCursor cursor = history.takeLast();
+
+            if(cursor.currentTable() != 0)
+                cursor.currentTable()->removeRows(0,1);
+            else {
+                cursor.select(QTextCursor::LineUnderCursor);
+                cursor.removeSelectedText();
+            }
+
+            moveCursor(QTextCursor::End);
+      }
     }
 };
 
